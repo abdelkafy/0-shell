d32743b66f13190ddf03fbbd56492a5a7c530983
abourazz <abdelkafy.bourazza.2@gmail.com>
2026-08-16 21:55:02 +0100

    fixed some merge issues v2

--- src/cmd_manager/executor.rs
+++ src/cmd_manager/executor.rs
@@ -1,17 +1,17 @@
-use crate::{cmd_manager::managers::{cat_manager, cd_manager, cp_manager, echo_manager, ls_manager, mkdir_manager, mv_manager, pwd_manager, rm_manager}, cmd_runner::mv, models::Command};
+use crate::{cmd_manager::managers , models::Command};
 
 
 pub fn command_executor(command:Command,args :Vec<String>){
     match command {
-        Command::Ls=>ls_manager(args),
-        Command::Pwd=>pwd_manager(),
-        Command::Cat=>cat_manager(args),
-        Command::Cp=>cp_manager(args),
-        Command::Cd=>cd_manager(args),
-        Command::Echo=>echo_manager(args),
-        Command::Mkdir=>mkdir_manager(args),
-        Command::Mv=>mv_manager(args),
-        Command::Rm=>rm_manager(args),
+        Command::Ls=>managers::ls_manager(args),
+        Command::Pwd=>managers::pwd_manager(),
+        Command::Cat=>managers::cat_manager(args),
+        Command::Cp=>managers::cp_manager(args),
+        Command::Cd=>managers::cd_manager(args),
+        Command::Echo=>managers::echo_manager(args),
+        Command::Mkdir=>managers::mkdir_manager(args),
+        Command::Mv=>managers::mv_manager(args),
+        Command::Rm=>managers::rm_manager(args),
         _=>print!("others"),
     }
 }
